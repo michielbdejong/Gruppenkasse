fccd76438bca10f2ae896102dfcae2190cece98c
xMartin <xmartin@siarp.de>
2012-10-22 22:56:47 +0200

    list of transactions

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 var App = Em.Application.create();
 
-App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
+App.TransactionsView = Em.View.extend();
+
+App.Transaction = Em.Object.extend();
+
+App.TransactionsController = Em.ArrayController.extend({
+  content: [],
+  init: function(){
+    this._super();
+    var json = '{"id":"1348302935202","box":"test","type":"spending","title":"test","date":1348264800000,"participants":["er","sie"],"payments":[{"participant":"er","amount":54}]}';
+    var transaction = App.Transaction.create(JSON.parse(json));
+    this.pushObject(transaction);
   }
 });
+
+App.transactionsController = App.TransactionsController.create();
